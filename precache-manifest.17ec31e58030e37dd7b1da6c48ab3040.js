b4e1bb6e77661be159efe6dba37c84c123102203
KirillKorneev <45182441+KirillKorneev@users.noreply.github.com>
2020-09-23 20:12:53 +0300

    Updates

---
+++ precache-manifest.17ec31e58030e37dd7b1da6c48ab3040.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c53291d6a76fc94a07505d079b5e42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75cbf4ce32811d7c6e22",
+    "url": "./static/css/main.fa90126b.chunk.css"
+  },
+  {
+    "revision": "344e1c5d85036c0cbc37",
+    "url": "./static/js/2.f4cdf0d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f4cdf0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cbf4ce32811d7c6e22",
+    "url": "./static/js/main.c2a91775.chunk.js"
+  },
+  {
+    "revision": "843c070479aa6cbf11308f81303859d3",
+    "url": "./static/js/main.c2a91775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "./static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "0bc377a472921b7200ebfad5fb4a5e82",
+    "url": "./static/media/Inter-ExtraBold.0bc377a4.woff"
+  },
+  {
+    "revision": "66f0ef6196f8d81ced9930f6675035a1",
+    "url": "./static/media/Inter-Medium.66f0ef61.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "./static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "2a9832ce927763768e66da8bd5eb7697",
+    "url": "./static/media/add_button.2a9832ce.svg"
+  },
+  {
+    "revision": "759f23c159edc3b7ebd9b081019e0f5a",
+    "url": "./static/media/add_button_big.759f23c1.svg"
+  },
+  {
+    "revision": "329d8ae338a75d4f1b996c3f2f1d7a4e",
+    "url": "./static/media/avatar.329d8ae3.jpg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "./static/media/delete.efa6fb04.svg"
+  },
+  {
+    "revision": "c8252db2902c59bff2cb619de2b75c2f",
+    "url": "./static/media/edit_button.c8252db2.svg"
+  },
+  {
+    "revision": "c41e84fdcd2e705523879b3deeb77d91",
+    "url": "./static/media/edit_button_min.c41e84fd.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "./static/media/header_logo.855a8c98.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "./static/media/like.6c74d9b5.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "./static/media/like_black.9fdebb6b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mesto-react/precache-manifest.b2bdda4ea5476e6a81de330d6af38962.js"
+  "./precache-manifest.17ec31e58030e37dd7b1da6c48ab3040.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mesto-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
